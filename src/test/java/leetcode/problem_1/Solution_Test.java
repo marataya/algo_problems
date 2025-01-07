778e4ad8fdcc1c4e107bf7ee1a7994fc162b1e84
Marat Ayaibergenov <marataya@gmail.com>
2025-01-07 21:27:57 +0500

    solution: 1: TwoSum

---
+++ src/test/java/leetcode/problem_1/Solution_Test.java
@@ -0,0 +1,21 @@
+package leetcode.problem_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_Test {
+
+    @Test
+    void twoSum() {
+        Solution_1 solution1 = new Solution_1();
+        Solution_2 solution2 = new Solution_2();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+        int[] result1 = solution1.twoSum(nums, target);
+        int[] result2 = solution2.twoSum(nums, target);
+        assertArrayEquals(expected, result1);
+        assertArrayEquals(expected, result2);
+    }
+}
