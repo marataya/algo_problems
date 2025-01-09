2c0131522e3d009cf871a28ccc9a773cd56e4edf
Marat Ayaibergenov <marataya@gmail.com>
2025-01-09 22:33:23 +0500

    Problem 2: add two linked lists

---
+++ src/test/java/leetcode/problem_2/SolutionTest.java
@@ -0,0 +1,48 @@
+package leetcode.problem_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void testAddTwoNumbersOfEqualLength() {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        assertEquals(7, result.val);
+        assertEquals(0, result.next.val);
+        assertEquals(8, result.next.next.val);
+        assertNull(result.next.next.next);
+    }
+
+    @Test
+    void testAddTwoListsWithOneZeroValues() {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(0);
+        ListNode l2 = new ListNode(0);
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        assertEquals(0, result.val);
+        assertNull(result.next);
+    }
+
+    @Test
+    void testAddTwoNumbersOfUnequalLength() {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        assertEquals(8, result.val);
+        assertEquals(9, result.next.val);
+        assertEquals(9, result.next.next.val);
+        assertEquals(9, result.next.next.next.val);
+        assertEquals(0, result.next.next.next.next.val);
+        assertEquals(0, result.next.next.next.next.next.val);
+        assertEquals(0, result.next.next.next.next.next.next.val);
+        assertEquals(1, result.next.next.next.next.next.next.next.val);
+        assertNull(result.next.next.next.next.next.next.next.next);
+    }
+
+}
